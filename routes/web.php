e4224962ae49022fc24b7840053adc4b05cd7e6d
dinushchathurya <dinushchathurya21@gmail.com>
2021-07-09 20:54:13 +0530

    feat: add required routes for login with Facebook

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('auth/facebook', 'SocialController@facebookRedirect');
+Route::get('auth/facebook/callback', 'SocialController@loginWithFacebook');
